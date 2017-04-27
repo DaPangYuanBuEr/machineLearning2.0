34aff3604b9f9979de65e0f8e59cc3d796194389
XiaoYuan [肖源] <XiaoYuan1@kingsoft.cn>
2017-04-27 22:42:17 +0800

    的

--- excise/newsclassification/t_V2.py
+++ excise/newsclassification/t_V2.py
@@ -6,8 +6,10 @@
 import jieba
 import numpy
 from sklearn import metrics
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer,CountVectorizer,\
+    TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
 
 
@@ -38,11 +40,23 @@ def input_data(train_file, test_file):
 comma_tokenizer = lambda x: jieba.cut(x, cut_all=True)
 
 
-def vectorize(train_words, test_words):
-    v = HashingVectorizer(tokenizer=comma_tokenizer, n_features=10, non_negative=True)
-    train_data = v.fit_transform(train_words)
-    test_data = v.fit_transform(test_words)
-    return train_data, test_data
+# def vectorize(train_words, test_words):
+#     v = HashingVectorizer(tokenizer=comma_tokenizer, n_features=10, non_negative=True)
+#     train_data = v.fit_transform(train_words)
+#     test_data = v.fit_transform(test_words)
+#     return train_data, test_data
+
+
+def vectorize(train_words,test_words):
+    count_vect = CountVectorizer()
+    train_counts = count_vect.fit_transform(train_words)
+    tfidf_trainsformer = TfidfTransformer()
+    train_tfidf = tfidf_trainsformer.fit_transform(train_counts)
+    
+    count_vect2 = CountVectorizer(vocabulary=count_vect.vocabulary_)
+    count_vect2 = count_vect2.fit_transform(test_words)
+    test_tfidf = tfidf_trainsformer.fit_transform(count_vect2)
+    return train_tfidf,test_tfidf
 
 
 def evaluate(actual, pred):
@@ -57,6 +71,11 @@ def train_clf(train_data, train_tags):
     clf.fit(train_data, numpy.asarray(train_tags))
     return clf
 
+def train_clfSVM(train_data,train_tags):
+    clf = SVC(kernel='linear')
+    clf.fit(train_data,train_tags)
+    return clf
+
 
 def main():
     train_file = 'data/train'
@@ -66,6 +85,10 @@ def main():
     clf = train_clf(train_data, train_tags)
     pred = clf.predict(test_data)
     evaluate(numpy.asarray(test_tags), pred)
+    clf = train_clf(train_data, train_tags)
+    pred = clf.predict(test_data)
+    evaluate(numpy.asarray(test_tags), pred)
+
 
 
 if __name__ == '__main__':
